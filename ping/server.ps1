2d1909897af0d5005c40900cdda8bd09e14e0715
bgelov <belov@ymail.com>
2023-05-28 22:34:27 -0300

    Add ping app

---
+++ ping/server.ps1
@@ -0,0 +1,7 @@
+﻿Start-PodeServer {
+    Add-PodeEndpoint -Address localhost -Port 8080 -Protocol Http
+
+    Add-PodeRoute -Method Get -Path '/ping' -ScriptBlock {
+        Write-PodeJsonResponse -Value @{ 'value' = 'pong' }
+    }
+}
