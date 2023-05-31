29f60d3a6c0dc91dd67a85ca644aab28ef5ea3d7
bgelov <belov@ymail.com>
2023-05-31 00:06:51 -0300

    Delete old code

--- ping/server.ps1
+++
@@ -1,7 +0,0 @@
-﻿Start-PodeServer {
-    Add-PodeEndpoint -Address localhost -Port 8080 -Protocol Http
-
-    Add-PodeRoute -Method Get -Path '/ping' -ScriptBlock {
-        Write-PodeJsonResponse -Value @{ 'value' = 'pong' }
-    }
-}
